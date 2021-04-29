ea7389e94e4144c399e2991fdbee0c4dcd8c42e2
utnso <emiliano.guiggiaro@gmail.com>
2021-04-28 21:20:47 -0300

    Un commit
    Un commit

--- store/src/store.c
+++ store/src/store.c
@@ -4,4 +4,6 @@ int main(){
     t_log* logger = log_create("./cfg/store.log", "STORE", true, LOG_LEVEL_INFO);
     log_info(logger, "Soy el store! %s", mi_funcion_compartida());
     log_destroy(logger);
+
+	printf("Hello world");
 }
